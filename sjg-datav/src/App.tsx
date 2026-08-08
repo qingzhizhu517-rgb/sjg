271c584a17366e748db45ce7174855c3beb27259
qingzhizhu517-rgb <qingzhizhu517-rgb@users.noreply.github.com>
2026-08-09 00:44:40 +0800

    feat(datav): 初始化项目结构和技术栈配置

---
+++ sjg-datav/src/App.tsx
@@ -0,0 +1,7 @@
+import DataV from './pages/DataV'
+
+function App() {
+  return <DataV />
+}
+
+export default App
---
+++ sjg-datav/src/pages/DataV/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const DataV: React.FC = () => {
+  return (
+    <div style={{
+      width: '100%',
+      height: '100%',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontSize: '2rem',
+      color: '#667eea'
+    }}>
+      齐鲁文化数据大屏 - 开发中
+    </div>
+  )
+}
+
+export default DataV
