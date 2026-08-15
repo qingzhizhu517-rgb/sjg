9698019bd5e6571382972235b57cad82f6c50eb4
qingzhizhu517-rgb <qingzhizhu517-rgb@users.noreply.github.com>
2026-08-15 23:42:44 +0800

    style(home): 移除首页五大文化板块聚合区(已由顶部导航「文化长廊」独立页承载), 名城精选段上移衔接

--- display-v2/src/views/MapView.vue
+++ display-v2/src/views/MapView.vue
@@ -235,11 +235,6 @@
       </div>
     </section>
 
-    <!-- ===== S4.5: 文化长廊 · 五大板块聚合入口 ===== -->
-    <section class="sn-section sn-gallery">
-      <CulturalGallery />
-    </section>
-
     <!-- ===== S5: 名城精选 ===== -->
     <section class="sn-section sn-featured">
       <FamousCities :cities="featuredCityData" @go="(route) => $router.push(route)" />
@@ -277,7 +272,6 @@ import RiverHero from '../components/homepage/RiverHero.vue'
 import CityDetailCard from '../components/homepage/CityDetailCard.vue'
 import StatTicker from '../components/homepage/StatTicker.vue'
 import FamousCities from '../components/homepage/FamousCities.vue'
-import CulturalGallery from '../components/homepage/CulturalGallery.vue'
 import FooterCTA from '../components/homepage/FooterCTA.vue'
 import { useCityEnrichment } from '../composables/useCityEnrichment'
 import { useThreeSandbox } from '../composables/useThreeSandbox'
